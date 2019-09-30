9a2c5365cb417aa8c3a8b5e2a25e7e4b445bf68b
Jack <mail@jackarrington.com>
2019-09-30 15:27:05 -0400

    Change port AGAIN

--- server.js
+++ server.js
@@ -3,13 +3,7 @@ const express = require("express")
 const app = express()
 app.use(express.static('./'))
 
-const env = process.env.NODE_ENV || 'dev'
-// let serverPort = 8081
-// if (env === 'dev') {
-//   serverPort = 3000
-// }
-
-const server = app.listen(8081, function(){
+const server = app.listen(process.env.PORT || 8081, function(){
   const port = server.address().port
   console.log("Server started at http://localhost:%s", port)
 })
