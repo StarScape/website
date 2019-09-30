949fdc987071698a2cb7c3cb8e195cedbbc0ee0f
Jack <mail@jackarrington.com>
2019-09-30 15:05:23 -0400

    Update bundle

--- src/scroll_anchors.js
+++ src/scroll_anchors.js
@@ -1,5 +1,5 @@
 // Smooth scrolling for anchor links
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('load', () => {
   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
       e.preventDefault();
