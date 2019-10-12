27b2d55cdf9b4c238b423c25c0b90d542a7d3bb5
Jack <mail@jackarrington.com>
2019-10-12 18:21:03 -0400

    Keep heroku app awake by pinging every 5 minutes

--- server.js
+++ server.js
@@ -7,3 +7,9 @@ const server = app.listen(process.env.PORT || 8081, function(){
   const port = server.address().port
   console.log("Server started at http://localhost:%s", port)
 })
+
+// Ping server every 5 minutes in production
+if (process.env.NODE_ENV === 'production') {
+  const http = require("http")
+  setInterval(() => http.get("http://jackarrington.com"), 300000)
+}
