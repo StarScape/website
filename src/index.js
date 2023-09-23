503c4f0acd18197308bdd9e90060d204b519c86f
Jack Arrington <mail@jackarrington.com>
2023-09-23 16:22:59 -0500

    Wording

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ import './stars.js'
 import './scroll.js'
 import './scroll_anchors.js'
 import './resize.js'
+
+
+function prettyPrint(msg) {
+  console.log(`%c${msg}`, 'color: #282f38; font-size: 13px;')
+}
+
+prettyPrint('Bruh. Pal. Buddy. Are you seriously looking at the source code for some random personal home page? How bored *are* you?')
+prettyPrint("It's a gigantic mess in here. I built this in like a couple days when I was a stressed-out college senior without the time or inclination for best practices, then forgot about it for years and later stripped it down to be something else (now with inclination for best practices, but not time)")
+prettyPrint("Have a peak, I guess? If you're just trying to embarrass me or something, sheesh...")
